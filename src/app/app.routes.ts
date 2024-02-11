ebb5fc0f736b0a31c594747891a68601589d911d
jxocoyro <jose.antonio.xocoy.rosales@ni.com>
2024-02-10 22:11:53 -0600

    feat: Routes
    * Agrega componentes Login, Landing & PageNotFound
    * Crea rutas iniciales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {LandingComponent} from "./landing/landing.component";
+import {LoginComponent} from "./login/login.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingComponent},
+    { path: 'login', component: LoginComponent},
+    { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
